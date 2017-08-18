7cca89d4887750014f9c2d9fe813e5dcc49bb823
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    init: Update for O
    Change-Id: Ia5b075b3a628a7af5ca72fae611237b7a4e2bb61

--- init/Android.mk
+++ init/Android.mk
@@ -22,5 +22,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_SRC_FILES := init_leeco_msm8996.cpp
 LOCAL_MODULE := libinit_leeco_msm8996
+LOCAL_STATIC_LIBRARIES += libbase
 
 include $(BUILD_STATIC_LIBRARY)
