4cf4c6d764a95b118f607ecab489500ae5dd7a0b
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:14:55 +0200

    Build Keymaster HAL
    Change-Id: Id316ca5b361cca7c5d1bb905d48b96d4601576fb

--- msm8996.mk
+++ msm8996.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes
 
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8996 \
