042d6bb28b012396fbf3ada26c8e1e7da26a7a09
Luca Stefani <luca020400@lineageos.org>
2017-09-07 21:01:58 +0300

    x2: Update display/render for O * Build the various display/render related HIDL HALs * Enable HWC2 & Gralloc1
    Change-Id: I7f0029331237f44bea6714313054f27bbb24fe56

--- msm8996.mk
+++ msm8996.mk
@@ -143,12 +143,14 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8996 \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
     memtrack.msm8996 \
-    liboverlay \
-    libtinyxml
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/FOSSConfig.xml:system/etc/FOSSConfig.xml
@@ -240,7 +242,12 @@ PRODUCT_PACKAGES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libtinyxml
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # RIL
 PRODUCT_PACKAGES += \
