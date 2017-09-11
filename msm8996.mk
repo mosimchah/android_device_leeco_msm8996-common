9935bcc98c58eaec70007ee2831ed2011fcd977b
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-11 22:09:51 +0200

    msm8996: seccomp: update policies from caf
    Change-Id: I4621ada8a1b1fa74ab779620eceee19734d13ea8

--- msm8996.mk
+++ msm8996.mk
@@ -271,6 +271,11 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
